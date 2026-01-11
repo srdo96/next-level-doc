9694ba9dde8427b3c4682dbc140fac24bce53b69
Sakib Rahman <seu.sakib@gmail.com>
2026-01-11 12:30:26 +0600

    fix text

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,111 +10,123 @@ import { themes as prismThemes } from "prism-react-renderer";
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "Next Level Doc",
-  tagline: "Next Level Doc",
-  favicon: "img/favicon.ico",
+    title: "Next Level Doc",
+    tagline: "Next Level Doc",
+    favicon: "img/favicon.ico",
 
-  // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
-  future: {
-    v4: true, // Improve compatibility with the upcoming Docusaurus v4
-  },
+    // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
+    future: {
+        v4: true, // Improve compatibility with the upcoming Docusaurus v4
+    },
 
-  markdown: {
-    mermaid: true,
-  },
-  themes: ["@docusaurus/theme-mermaid"],
+    markdown: {
+        mermaid: true,
+    },
+    themes: ["@docusaurus/theme-mermaid"],
 
-  // Set the production url of your site here
-  url: "https://srdo96.github.io",
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/next-level-doc/",
+    // Set the production url of your site here
+    url: "https://srdo96.github.io",
+    // Set the /<baseUrl>/ pathname under which your site is served
+    // For GitHub pages deployment, it is often '/<projectName>/'
+    baseUrl: "/next-level-doc/",
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: "srdo96", // Usually your GitHub org/user name.
-  projectName: "next-level-doc", // Usually your repo name.
+    // GitHub pages deployment config.
+    // If you aren't using GitHub pages, you don't need these.
+    organizationName: "srdo96", // Usually your GitHub org/user name.
+    projectName: "next-level-doc", // Usually your repo name.
 
-  onBrokenLinks: "throw",
-  deploymentBranch: "gh-pages",
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: "en",
-    locales: ["en"],
-  },
+    onBrokenLinks: "throw",
+    deploymentBranch: "gh-pages",
+    // Even if you don't use internationalization, you can use this field to set
+    // useful metadata like html lang. For example, if your site is Chinese, you
+    // may want to replace "en" with "zh-Hans".
+    i18n: {
+        defaultLocale: "en",
+        locales: ["en"],
+    },
 
-  presets: [
-    [
-      "classic",
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
-        docs: {
-          sidebarPath: "./sidebars.js",
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl: "https://github.com/srdo96/next-level-doc",
-        },
-        blog: {
-          showReadingTime: true,
-          feedOptions: {
-            type: ["rss", "atom"],
-            xslt: true,
-          },
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl: "https://github.com/srdo96/next-level-doc",
-          // Useful options to enforce blogging best practices
-          onInlineTags: "warn",
-          onInlineAuthors: "warn",
-          onUntruncatedBlogPosts: "warn",
-        },
-        theme: {
-          customCss: "./src/css/custom.css",
-        },
-      }),
-    ],
-  ],
-
-  themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      // Replace with your project's social card
-      image: "img/docusaurus-social-card.png",
-      colorMode: {
-        respectPrefersColorScheme: true,
-      },
-      navbar: {
-        title: "Next Level Doc",
-        logo: {
-          alt: "next level doc logo",
-          src: "img/logo.png",
-        },
-        items: [
-          {
-            type: "docSidebar",
-            sidebarId: "tutorialSidebar",
-            position: "left",
-            label: "Docs",
-          },
-          {
-            href: "https://github.com/srdo96/next-level-doc",
-            label: "GitHub",
-            position: "right",
-          },
+    presets: [
+        [
+            "classic",
+            /** @type {import('@docusaurus/preset-classic').Options} */
+            ({
+                docs: {
+                    sidebarPath: "./sidebars.js",
+                    // Please change this to your repo.
+                    // Remove this to remove the "edit this page" links.
+                    editUrl: "https://github.com/srdo96/next-level-doc",
+                },
+                blog: {
+                    showReadingTime: true,
+                    feedOptions: {
+                        type: ["rss", "atom"],
+                        xslt: true,
+                    },
+                    // Please change this to your repo.
+                    // Remove this to remove the "edit this page" links.
+                    editUrl: "https://github.com/srdo96/next-level-doc",
+                    // Useful options to enforce blogging best practices
+                    onInlineTags: "warn",
+                    onInlineAuthors: "warn",
+                    onUntruncatedBlogPosts: "warn",
+                },
+                theme: {
+                    customCss: "./src/css/custom.css",
+                },
+            }),
         ],
-      },
-      footer: {
-        style: "dark",
+    ],
 
-        copyright: `Copyright © ${new Date().getFullYear()} Next Level Doc. Built with ❤️`,
-      },
-      prism: {
-        theme: prismThemes.github,
-        darkTheme: prismThemes.dracula,
-      },
-    }),
+    themeConfig:
+        /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+        ({
+            // Replace with your project's social card
+            image: "img/docusaurus-social-card.png",
+            colorMode: {
+                respectPrefersColorScheme: true,
+            },
+            navbar: {
+                title: "Next Level Doc",
+                logo: {
+                    alt: "next level doc logo",
+                    src: "img/logo.png",
+                },
+                items: [
+                    {
+                        type: "docSidebar",
+                        sidebarId: "tutorialSidebar",
+                        position: "left",
+                        label: "Docs",
+                    },
+                    // {
+                    //     type: "docSidebar",
+                    //     sidebarId: "aiNotesSidebar",
+                    //     position: "left",
+                    //     label: "AI Notes",
+                    // },
+                    // {
+                    //     type: "docSidebar",
+                    //     sidebarId: "resourcesSidebar",
+                    //     position: "left",
+                    //     label: "Resources",
+                    // },
+                    {
+                        href: "https://github.com/srdo96/next-level-doc",
+                        label: "GitHub",
+                        position: "right",
+                    },
+                ],
+            },
+            footer: {
+                style: "dark",
+
+                copyright: `Copyright © ${new Date().getFullYear()} Next Level Doc. Built with ❤️`,
+            },
+            prism: {
+                theme: prismThemes.github,
+                darkTheme: prismThemes.dracula,
+            },
+        }),
 };
 
 export default config;
